5849eba661958d46c018b86e9c8fed712bc13271
Sereina Blanchard <wintermist3@gmail.com>
2017-10-03 12:41:13 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Music Player is a UX project to create an interface to play mp3's. 
+
+__Built With:__
+* HTML
+* CSS
+* JavaScript
+* jQuery
+
+![Media Player](https://github.com/Ziyal/Music-Player/blob/master/screenshot.png "Media Player")
---
+++ script.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    
+    var audio = document.getElementById("audio");
+
+    audio.ontimeupdate = function() {
+        currTime()
+    }
+
+    function currTime() {
+
+        // Converts currentTime into m:ss format
+        if(audio.currentTime > 59) {
+            var minutes = Math.floor(audio.currentTime/60);
+            var seconds = audio.currentTime - minutes * 60;
+
+        } if(audio.currentTime < 10) {
+            var minutes = 0;
+            var seconds = "0" + Math.round(audio.currentTime);
+        } else {
+            var minutes = 0;
+            var seconds = Math.round(audio.currentTime);
+        }
+
+        console.log(minutes + ":" + seconds);
+
+        $(".curr-time").empty();
+        $(".curr-time").append(minutes + ":" + seconds);
+    }
+    
+    
+    function play() {
+        console.log("Play hit");
+        audio.play();
+
+        $("#p-btn").empty();
+        $("#p-btn").append("<span class='pause'>&#9208;</span>");
+        $("#p-btn").removeClass("play-btn");
+        $("#p-btn").addClass("pause-btn");
+        
+    }
+
+
+    function pause() {
+        console.log("Pause hit");
+        audio.pause();
+
+        $("#p-btn").empty();
+        $("#p-btn").append("<span class='play'>&#9654;</span>");
+        $("#p-btn").removeClass("pause-btn");
+        $("#p-btn").addClass("play-btn");
+    }
+
+    $(document).on("click", ".play-btn", play);
+    $(document).on("click", ".pause-btn", pause);
+
+    currTime();
+
+});
